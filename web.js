73b1b2b7a58bf5842afbe2109dd7f7fd70cb9019
Ken Chu <chu.kenneth.y@gmail.com>
2013-07-10 03:41:25 +0000

    first pass at reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
+var fs = require('fs');
+var infile = "index.html";
+var inbuf = fs.readFileSync(infile);
+var instring = inbuf.toString();
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(instring);
 });
 
 var port = process.env.PORT || 5000;
